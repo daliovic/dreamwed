54ad345a4db61059bd047fb6f126c484e77c2cc6
Mohamed Ali Ben Thaier <dalidaliovic@gmail.com>
2022-11-05 15:29:00 +0300

    Revert "Guest Refactor"
    This reverts commit a1245afe3240ad8bef7257002f3b01e2873b27f6.

--- src/components/InvitationDetailItem.js
+++ src/components/InvitationDetailItem.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import { Button, Dropdown } from 'react-bootstrap'
-//import reset icon
-import { BsArrowRepeat } from 'react-icons/bs'
+import { Dropdown } from 'react-bootstrap'
 const STYLE_PROPERTIES = [
     "*LAYOUT*",
     "bottom",
@@ -101,87 +99,72 @@ const STYLE_PROPERTIES = [
     "borderBottomLeftRadius",
     "borderRadius",
 ]
-export default function InvitationDetailItem({ invitationDetails, setInvitationDetails, detailItem, adv }) {
+export default function InvitationDetailItem({invitationDetails, setInvitationDetails , detailItem, adv}) {
     const showClass = !adv ? "d-none" : "d-flex"
+  return (
+    <div>
+                    <div className="mb-2">
+                        <span className="col-4" style={{textTransform: "capitalize"}}>{detailItem}</span>
+                        <input 
+                        placeholder={invitationDetails[detailItem].name}
+                        className='form-control' 
+                        onBlur={(e) => { setInvitationDetails({ ...invitationDetails, [detailItem]: { ...invitationDetails[detailItem], name: e.target.value } }) }} />
+                    </div>
+                    <div className={`mb-4 ${showClass} `} >
+                        <div className='col-4 '>
+                            <span className="col-4">Property Name</span>
 
-    const resetStylesHandler = () => {
-        setInvitationDetails(prev => ({...prev, [detailItem]: {...prev[detailItem], styles: {}}}))
-    }
+                            <Dropdown className='col-12 me-4'>
+                                <Dropdown.Toggle
+                                    className='col-12 text-start'
+                                    style={{ backgroundColor: "#FFF", border: "1px solid #ced4da", color: "#777" }} id="dropdown-basic">
+                                    {invitationDetails[detailItem].styles.currentProp}
+                                </Dropdown.Toggle>
 
-    return (
-        <div>
-            <div className="mb-2">
-                <span className="col-4" style={{ textTransform: "capitalize" }}>{detailItem}</span>
-                <input
-                    placeholder={invitationDetails[detailItem].name}
-                    className='form-control'
-                    onChange={(e) => setInvitationDetails(prev => ({ ...prev, [detailItem]: { ...prev[detailItem], name: e.target.value } }))}
-                    value={invitationDetails[detailItem].name}
-                    // onBlur={(e) => { setInvitationDetails({ ...invitationDetails, [detailItem]: { ...invitationDetails[detailItem], name: e.target.value } }) }}
-                     />
+                                <Dropdown.Menu
 
-            </div>
-            <div className={`mb-4 ${showClass} `} >
-                <div className='col-4 '>
-                    <span className="col-4">Property Name</span>
+                                    style={{
+                                        height: "250px",
+                                        overflowY: 'scroll'
+                                    }}>
+                                    {STYLE_PROPERTIES.map((item, i) => {
 
-                    <Dropdown className='col-12 me-4'>
-                        <Dropdown.Toggle
-                            className='col-12 text-start'
-                            style={{ backgroundColor: "#FFF", border: "1px solid #ced4da", color: "#777" }} id="dropdown-basic">
-                            {invitationDetails[detailItem].styles.currentProp}
-                        </Dropdown.Toggle>
-
-                        <Dropdown.Menu
-
-                            style={{
-                                height: "250px",
-                                overflowY: 'scroll'
-                            }}>
-                            {STYLE_PROPERTIES.map((item, i) => {
-
-                                if (i === 0) {
-                                    return (
-                                        <Dropdown.Item disabled={true} key={item} className="border-bottom">{item}</Dropdown.Item>
-                                    )
-                                }
-                                if (item.includes("*"))
-                                    return (
-                                        <Dropdown.Item disabled={true} key={item} className="border-bottom border-top">{item}</Dropdown.Item>
-                                    )
-                                let color = ""
-                                invitationDetails[detailItem].styles[item] ? color = "#e6ffe6" : color = "inherit"
-                                return (
-                                    <Dropdown.Item
-                                        style={{ width: "100%", backgroundColor: color }}
-                                        onClick={() => { setInvitationDetails({ ...invitationDetails, [detailItem]: { ...invitationDetails[detailItem], styles: { ...invitationDetails[detailItem].styles, currentProp: item } } }) }}
-                                        key={item}>{item}
-                                    </Dropdown.Item>)
-                            })}
-                        </Dropdown.Menu>
-                    </Dropdown>
-                </div>
-                <div className='col-8 ps-4'>
-                    <span className="col-4">Property Value</span>
-                    <div className='col-12 d-flex'>
-                        <div className='col-10 pe-4'>
-
+                                        if (i === 0) {
+                                            return (
+                                                <Dropdown.Item disabled={true} key={item} className="border-bottom">{item}</Dropdown.Item>
+                                            )
+                                        }
+                                        if (item.includes("*"))
+                                            return (
+                                                <Dropdown.Item disabled={true} key={item} className="border-bottom border-top">{item}</Dropdown.Item>
+                                            )
+                                        let color = ""
+                                        invitationDetails[detailItem].styles[item] ? color = "#e6ffe6" : color = "inherit"
+                                        return (
+                                            <Dropdown.Item
+                                                style={{ width: "100%", backgroundColor: color }}
+                                                onClick={() => { setInvitationDetails({ ...invitationDetails, [detailItem]: { ...invitationDetails[detailItem], styles: { ...invitationDetails[detailItem].styles, currentProp: item } } }) }}
+                                                key={item}>{item}
+                                            </Dropdown.Item>)
+                                    })}
+                                </Dropdown.Menu>
+                            </Dropdown>
+                        </div>
+                        <div className='col-8 ps-4'>
+                            <span className="col-4">Property Value</span>
                             <input
                                 className='form-control'
+                                // placeholder={groomSt[groomSt.currentProp]}
                                 placeholder={
+                                    // (groomSt[groomSt.currentProp] && !groomSt.currentProp.includes("Select")) ?
                                     (invitationDetails[detailItem].styles[invitationDetails[detailItem].styles.currentProp] && !invitationDetails[detailItem].styles.currentProp.includes("Select")) ?
                                         `Current Value: ${invitationDetails[detailItem].styles[invitationDetails[detailItem].styles.currentProp]}` :
                                         "(default)"}
+                                // onBlur={(e) => { setGroomSt({ ...groomSt, [groomSt.currentProp]: `${e.target.value}` }) }} />
                                 onBlur={(e) => { setInvitationDetails({ ...invitationDetails, [detailItem]: { ...invitationDetails[detailItem], styles: { ...invitationDetails[detailItem].styles, [invitationDetails[detailItem].styles.currentProp]: `${e.target.value}` } } }) }} />
                         </div>
-                        <Button variant='danger' className='col-2 px-0' onClick={resetStylesHandler}>
-                            <BsArrowRepeat /> 
-                        </Button>
+
                     </div>
                 </div>
-
-
-            </div>
-        </div>
-    )
+  )
 }
